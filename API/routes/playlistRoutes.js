57a89bf2d6f8e15ce73d5860abed5610f2ba199c
Joost Kaal <joost.kaal@hotmail.nl>
2020-01-22 14:04:34 +0100

    default route

--- API/routes/playlistRoutes.js
+++ API/routes/playlistRoutes.js
@@ -2,6 +2,9 @@ module.exports = function(app) {
     var playlistController = require('../controllers/playlistController');
 
     // Routes
+    app.route('/')
+        .get(res.status(200).send('Welkom op de API van gruusbekify'));
+
     app.route('/playlist/:id')
         .get(playlistController.getPlaylist);
 
