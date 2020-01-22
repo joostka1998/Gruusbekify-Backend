cfa866c47d6810dd4470a7a7940cb4bd5015538f
Joost Kaal <joost.kaal@hotmail.nl>
2020-01-22 13:54:33 +0100

    test

--- API/controllers/playlistController.js
+++ API/controllers/playlistController.js
@@ -4,7 +4,7 @@ var connection = new ConnectionFactory();
 exports.getPlaylist = async function (req, res) {
     try {
         result = await connection.executeQuery('SELECT * FROM Playlists WHERE playlistID = ' + req.params.id);
-        res.status(200).send(result);
+        res.status(200).send(result + 'TEST');
     } catch (err) {
         console.log(err);
     }
